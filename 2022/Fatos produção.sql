3d4681924c2cb294e539f9f2664060d5c5685d79
demitredacol <demitre.dacol@gmail.com>
2023-07-15 08:34:56 -0300

    Ajustes no fato de produção.

--- 2022/Fatos produção.sql
+++ 2022/Fatos produção.sql
@@ -7,18 +7,20 @@ GO
 -- ORDENS E OPERAÇÕES
 SELECT WorkOrder.WorkOrderID Ordem,
 	   WorkOrder.ProductID CodigoProduto,
-	   WorkOrder.OrderQty Quantidade,
-	   WorkOrder.StartDate "Data início ordem",
-	   WorkOrder.DueDate "Prazo de entrega",
-	   WorkOrder.EndDate "Data encerramento ordem",
+	   WorkOrder.OrderQty "Quantidade solicitada",
+	   WorkOrder.StockedQty "Quantidade produzida",
+	   WorkOrder.ScrappedQty "Quantidade refugada",
+	   DATEADD(YEAR, 9, WorkOrder.StartDate) "Data início ordem",
+	   DATEADD(YEAR, 9, WorkOrder.DueDate) "Prazo de entrega",
+	   DATEADD(YEAR, 9, WorkOrder.EndDate) "Data encerramento ordem",
 	   WorkOrder.ScrapReasonID CodigoMotivoRefugo,
 	   WorkOrderRouting.OperationSequence "Operação",
 	   WorkOrderRouting.LocationID "CodigoLocal",
-	   WorkOrderRouting.ScheduledStartDate "Início planejado operação",
-	   WorkOrderRouting.ScheduledEndDate "Fim planejado operação",
+	   DATEADD(YEAR, 9, WorkOrderRouting.ScheduledStartDate) "Início planejado operação",
+	   DATEADD(YEAR, 9, WorkOrderRouting.ScheduledEndDate) "Fim planejado operação",
 	   WorkOrderRouting.PlannedCost "Custo planejado operação",
-	   WorkOrderRouting.ActualStartDate "Início real operação",
-	   WorkOrderRouting.ActualEndDate "Fim real operação",
+	   DATEADD(YEAR, 9, WorkOrderRouting.ActualStartDate) "Início real operação",
+	   DATEADD(YEAR, 9, WorkOrderRouting.ActualEndDate) "Fim real operação",
 	   WorkOrderRouting.ActualCost "Custo real operação"
   FROM Production.WorkOrder
   LEFT JOIN Production.WorkOrderRouting ON WorkOrder.WorkOrderID = WorkOrderRouting.WorkOrderID
@@ -29,10 +31,12 @@ SELECT WorkOrder.WorkOrderID Ordem,
 SELECT 
        TransactionHistory.TransactionID LancamentoId,
 	   TransactionHistory.ReferenceOrderID Ordem,
-	   TransactionHistory.TransactionDate Data,
+	   DATEADD(YEAR, 9, TransactionHistory.TransactionDate) Data,
 	   TransactionHistory.Quantity "Quantidade",
 	   TransactionHistory.ActualCost "Custo"
 FROM Production.TransactionHistory  
 WHERE TransactionType = 'W'
 
 */
+
+select top 10 * from Production.WorkOrder
